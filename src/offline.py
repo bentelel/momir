3528b6632bcb04e3c7e6f18139caec632685c99c
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2026-02-18 14:42:31 +0100

    worked on offline.py + moved meld cards into own config category because they are not like other flip cards

--- src/offline.py
+++ src/offline.py
@@ -7,21 +7,40 @@ with open('config.yaml', 'r') as f:
 
 class OfflineClient:
     randomCard = {}
+    cards = {}
+    filteredCards = {}
 
     def __init__(self):
         with open(f"cards/{CONFIG['offline']['card_json']}", 'r', encoding='utf-8') as f:
-            self.data = json.load(f)
+            self.cards = json.load(f)
         self.getRandomCard()
+        self.filterData()
 
     def getRandomCard(self):
-        self.randomCard = random.choice(self.data)
+        self.randomCard = random.choice(self.cards)
     
     def printRandomCard(self):
         print(self.randomCard)
 
-    def verifyCard(self):
-        pass
+    def verifyCard(self, card: dict) -> bool:
+        #method to verify a card is valid in the momir grab (manavalue, type_line?)
+        if card['set'].upper() in CONFIG['api_options']['sets_to_exclude']:
+            return False
+        if 'Creature' not in card['type_line']:
+            return False
+        if card['layout'] in CONFIG['api_options']['splitcard_layouts']: 
+            # need to nest this because the non-double sided cards do not have the card_faces key
+            if 'Creature' not in card['card_faces'][0]['type_line']:
+                return False
+        return True
+
+    def filterData(self):
+        #method to filter the loaded json to only include momir valid cards
+        self.filteredCards = [x for x in self.cards if self.verifyCard(x)]
+
 
 if __name__ == "__main__":
     o = OfflineClient()
     o.printRandomCard()
+    print(len(o.cards))
+    print(len(o.filteredCards))
