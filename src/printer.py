e78ca4d4c55b3dcf296d27022964562fbca56ff7
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2026-02-20 09:05:20 +0100

    POSPrinter class adjustments, config expansion

--- src/printer.py
+++ src/printer.py
@@ -1,5 +1,6 @@
 #class for printer access
 from escpos.printer import Dummy, Win32Raw
+import config
 
 PRINTER_NAME = 'POS-58'
 
@@ -18,26 +19,59 @@ def printSomeShit(shit: str) -> None:
     printer.text('  ') # placeholder so last row isnt cut? last line is still cut
     printer.close()
 
-def printImage(img) -> None:
-    printer = Win32Raw(PRINTER_NAME)
-    printer._raw(b'\x1b\x40')
-    optimizedImg = img
-    printer.set(align='left')
-    printer.image(optimizedImg, impl="bitImageRaster")
-    printer.cut()
-    printer.close()
+def printImage(self, img) -> None:
+        printer = Win32Raw(PRINTER_NAME)
+        printer._raw(b'\x1b\x40')
+        optimizedImg = img
+        printer.set(align='left')
+        printer.image(optimizedImg, impl="bitImageRaster")
+        printer.cut()
+        printer.close()
 
-def optimizedImg(img):
-    # Resize to printer width (max 384px for 58mm)
-    max_width = 384 #image currently is cropped now matter what I set here. Maybe need more printer profile.
-    if img.width > max_width:
-        ratio = max_width / float(img.width)
-        new_height = int((float(img.height) * float(ratio)))
-        img = img.resize((max_width, new_height))
-    # Convert to proper 1-bit black & white
-    img = img.convert("L")
-    img = img.point(lambda x: 0 if x < 128 else 255, '1')
-    return img
+    def optimizedImg(img):
+        # Resize to printer width (max 384px for 58mm)
+        max_width = 384 #image currently is cropped now matter what I set here. Maybe need more printer profile.
+        if img.width > max_width:
+            ratio = max_width / float(img.width)
+            new_height = int((float(img.height) * float(ratio)))
+            img = img.resize((max_width, new_height))
+        # Convert to proper 1-bit black & white
+        img = img.convert("L")
+        img = img.point(lambda x: 0 if x < 128 else 255, '1')
+        return img
+
+class POSPrinter:
+
+    def __init__(self):
+        #should probably pass the config info into this class and not pull it in here in order to allow more than 1 printer with different settings?
+        self.config=load_config() 
+        self.name=self.config.printer.win_printer_name
+        self.maxImgWidth=self.config.printer.max_img_width_in_px
+        self.imgPrintImpl=self.config.printer.img_print_implementation
+
+    def printText(self, text: src) -> None:
+        pass
+
+    def printImage(self, img) -> None:
+        printer = self.name
+        printer._raw(b'\x1b\x40')
+        optimizedImg = img
+        printer.set(align='left')
+        printer.image(optimizedImg, impl=self.imgPrintImpl)
+        printer.cut()
+        printer.close()
+
+    def optimizedImg(img):
+        # Resize to printer width (max 384px for 58mm)
+        #image currently is cropped now matter what I set here. Maybe need more printer profile.
+        if img.width > self.maxImgWidth:
+            ratio = max_width / float(img.width)
+            new_height = int((float(img.height) * float(ratio)))
+            img = img.resize((max_width, new_height))
+        # Convert to proper 1-bit black & white
+        img = img.convert("L")
+        img = img.point(lambda x: 0 if x < 128 else 255, '1')
+        return img
 
 if __name__=="__main__":
     try:
